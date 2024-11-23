879c416e87d0c89a423467a9ead18ceefc3e9685
Francisco Junior <jun.oliveira10@gmail.com>
2024-11-23 17:45:11 +0000

    Adding menuType

---
+++ Sources/HealthyIntelligenceComponent/Sandbox.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  HealthyIntelligenceComponent
+//
+//  Created by Junior Oliveira on 23/11/2024.
+//
+
+import SwiftUI
+
+struct Sandbox: View {
+    var body: some View {
+        
+        
+        
+        
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
