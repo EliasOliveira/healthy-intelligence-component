820154cd878506a263eb4d7d363260d339b11681
Francisco Junior <jun.oliveira10@gmail.com>
2024-11-23 16:29:21 +0000

    rc11

--- Sources/HealthyIntelligenceComponent/HealthyIntelligenceComponent.swift
+++ Sources/HealthyIntelligenceComponent/HealthyIntelligenceComponent.swift
@@ -3,6 +3,15 @@
 
 import SwiftUI
 
+public enum MenuItemType {
+    case image(Image)
+    case text(Text)
+}
+
+public func hello () {
+    print("Hello, World!")
+}
+
 @available(macOS 14, iOS 18, *)
 struct MenuItem: View {
     var mockImageUrl = "https://firebasestorage.googleapis.com/v0/b/streaminator-lives.appspot.com/o/assets%2Fchincoteague%402x.jpg?alt=media&token=b3ec1824-39b9-4358-9506-ce2551039650"
