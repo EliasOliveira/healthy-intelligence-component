4f76e88ae5b88898b9e89445d5e23d9bcd6a2b9c
Francisco Junior <jun.oliveira10@gmail.com>
2024-11-23 16:06:59 +0000

    rc9

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "HealthyIntelligenceComponent",
-            path: "Sources/Components"
+            path: "Sources/HealthyIntelligenceComponent"
         ),
 
     ]
