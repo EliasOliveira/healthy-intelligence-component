8238f72897341d72c6efe6d910f88e208f387f5a
Francisco Junior <jun.oliveira10@gmail.com>
2024-11-23 13:42:39 +0000

    Initial

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "healthy-intelligence-component",
-            targets: ["healthy-intelligence-component"]),
+            name: "HealthyIntelligenceComponent",
+            targets: ["HealthyIntelligenceComponent"]),
     ],
     
     dependencies: [
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "healthy-intelligence-component"),
+            name: "HealthyIntelligenceComponent"),
         .testTarget(
             name: "healthy-intelligence-componentTests",
-            dependencies: ["healthy-intelligence-component"]
+            dependencies: ["HealthyIntelligenceComponent"]
         ),
     ]
 )
