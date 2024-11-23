ddcb5f4bab477c7d86b9920eaf8d9768b35694b0
Francisco Junior <jun.oliveira10@gmail.com>
2024-11-23 15:01:42 +0000

    Change package

--- Package.swift
+++ Package.swift
@@ -4,29 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "healthy-intelligence-component",
-    platforms: [
-        .macOS(.v13), .iOS(.v16)
-    ],
+    name: "HealthyIntelligenceComponent",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "HealthyIntelligenceComponent",
             targets: ["HealthyIntelligenceComponent"]),
     ],
-    
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-    ],
-    
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "HealthyIntelligenceComponent"),
-        .testTarget(
-            name: "healthy-intelligence-componentTests",
-            dependencies: ["HealthyIntelligenceComponent"]
-        ),
+
     ]
 )
