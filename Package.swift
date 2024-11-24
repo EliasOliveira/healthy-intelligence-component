82515e8e6331540aa6adb12ac4c88e474657c24a
Francisco Junior <jun.oliveira10@gmail.com>
2024-11-24 14:18:10 +0000

    Initial chupa cabra

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "HealthyIntelligenceComponent",
     platforms: [
-        .macOS(.v14), .iOS(.v18)
+        .macOS(.v14), .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
